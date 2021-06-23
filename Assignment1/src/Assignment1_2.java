8d510388b07a17c5f568e3f138abbe1185c42f95
Barry Percy <baz.percy@gmail.com>
2021-06-23 05:28:15 +0100

    Assignment 2_2

---
+++ Assignment1/src/Assignment1_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Assignment1_2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What are you favourite fruits?");
+		String fruits = scan.nextLine();
+		System.out.println(fruits + " are great fruits!");
+	}
+}
